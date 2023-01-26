8055635be3cf3eeb011d2f5ec769a10042a92f3c
landigf <95249781+landigf@users.noreply.github.com>
2023-01-26 15:20:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/* JavaScript */
+
+function toggleNav() {
+    var nav = document.getElementById("nav");
+    if (nav.style.display === "block") {
+    nav.style.display = "none";
+    } else {
+    nav.style.display = "block";
+    }
+    }
